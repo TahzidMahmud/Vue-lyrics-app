8c0db48eb149f8e46c5eb97e2f23a0d6442afed5
tahzid_shuvo <tahzid15-1272@diu.edu.bd>
2020-09-20 00:21:52 +0600

    env file setup-linked

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld :msg="this.msg" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  created() {},
+  data() {
+    return {
+      msg: process.env.VUE_APP_STRING,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
